cbbaae702a9327f62213b2b5cfe6d5abff17af24
Vicky Adebona <vadebona@uci.edu>
2018-04-30 22:36:00 -0700

    started writing update_time function, updated main function with second increments

--- main.c
+++ main.c
@@ -8,6 +8,7 @@
 #include <avr/io.h>
 #include "avr.c"
 #include "lcd.c"
+#include <stdio.h>
 
 
 struct Clock {
@@ -21,6 +22,33 @@ struct Clock {
 
 };
 
+void update_time(Clock c, int s) {
+	c.second += 1;
+	
+	if(s % 60 == 0) {
+		c.minute += 1;
+	}
+	
+	if (s % 360 == 0) {
+		c.hour += 1;
+	}
+	
+	if (c.second == 60) {
+		c.second = 0;
+	}
+	
+	if(c.minute == 60) {
+		c.minute = 0;
+	}
+	
+	if(c.hour > 12) {
+		c.hour = 1;
+	}
+		
+}
+
+
+
 int is_pressed(int r, int c) {
 	// Set all R's and all C's to N/C
 	DDRC = 0;
@@ -32,7 +60,7 @@ int is_pressed(int r, int c) {
 	
 	// Set c to Weak 1
 	SET_BIT(DDRC, c+4); // Add 4 to reach pins 4-7 on keypad
-	CLR_BIT(PORTC, c+4);
+	//CLR_BIT(PORTC, c+4);
 	
 	// Change state of button
 	if(GET_BIT(PINC, c+4)) {
@@ -52,11 +80,23 @@ int get_key() {
 			}
 		}
 	}
+	
+	return 0;
 }
 
 int main()
 {
-    while (1) {
+	Clock c = {0, 0, 0, 0, 0, 0};
+	int total_seconds = 0;
+	
+	while(1) {
+		wait_avr(1000);
+		total_seconds += 1;
+		update_time(c, total_seconds);
+	}
+	
+    /*
+	while (1) {
 		int k = get_key();
 		
 		// blink led k times
@@ -69,7 +109,7 @@ int main()
 		
 		// wait 2 seconds
 		wait_avr(2000);
-    }
+    } */
 	
 	return 0;
 }
